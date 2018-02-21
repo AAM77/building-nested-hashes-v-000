7f611846e5c138f0ee27ca8c100f1a75efa435c2
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-21 20:44:12 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
         {},
         {}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {
         :name => " ",
@@ -40,5 +40,5 @@ def third_challenge
    }
   }
 
-  
+
 end
