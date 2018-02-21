4d5fdef24781b81115bb77c2495ef18fcba4169b
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-21 20:47:48 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,27 +6,35 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        :name => " ",
-        :age => " "
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        :name => " ",
-        :age => " ",
-        :status => " "
+        :name => "Romeo",
+        :age => "15",
+        :status => "Alive"
         },
       :hero_friends => [
-        {},
-        {}
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
       ]
    },
    :capulet => {
       :patriarch => {
-        :name => " ",
-        :age => " "
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
-        :name => " ",
-        :age => " "
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         :name => " ",
